56dc6560db42e5788fa8ceb32ef4bed5ae1ad99c
MateoAlvaradoS <kevin.alvarado@ucuenca.edu.ec>
2024-05-23 12:05:58 -0500

    Version 2

--- Practica 5 - Javascript/script.js
+++ Practica 5 - Javascript/script.js
@@ -1,9 +1,90 @@
 function agregarLocal (){
-    persona = {
+    localStorage.setItem(localStorage.length, JSON.stringify({
+        apellido: document.getElementById("txtApellido").value,
         nombre: document.getElementById("txtNombre").value,
         edad: document.getElementById("txtEdad").value
-    };
-    localStorage.setItem(1, JSON.stringify(persona));
+    }));
+    mostrarTablaLocal();
 }
 
 document.getElementById("btnLocalStorage").addEventListener('click', agregarLocal);
+
+
+function agregarSession (){
+    sessionStorage.setItem(sessionStorage.length, JSON.stringify({
+        apellido: document.getElementById("txtApellido").value,
+        nombre: document.getElementById("txtNombre").value,
+        edad: document.getElementById("txtEdad").value
+    }));
+    mostrarTablaSession();
+}
+
+document.getElementById("btnSessionStorage").addEventListener('click', agregarSession);
+
+
+
+function mostrarTablaLocal() {
+    var tabla = document.getElementById("tblLocal");
+    var tbody = tabla.getElementsByTagName("tbody")[0];
+
+    // Limpiar el contenido existente de la tabla
+    tbody.innerHTML = '';
+
+    // Iterar sobre el array de personas y agregar una fila por cada persona
+    for(let i=0;i <localStorage.length; i++){
+        let elemento = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        var fila = document.createElement("tr");
+
+        // Agregar celdas con los datos de la persona
+        var celdaApellido = document.createElement("td");
+        celdaApellido.textContent = elemento.apellido;
+        fila.appendChild(celdaApellido);
+
+        var celdaNombre = document.createElement("td");
+        celdaNombre.textContent = elemento.nombre;
+        fila.appendChild(celdaNombre);
+
+        var celdaEdad = document.createElement("td");
+        celdaEdad.textContent = elemento.edad;
+        fila.appendChild(celdaEdad);
+
+        // Agregar la fila a la tabla
+        tbody.appendChild(fila);
+    }
+}
+
+mostrarTablaLocal();
+
+function mostrarTablaSession() {
+    var tabla = document.getElementById("tblSession");
+    var tbody = tabla.getElementsByTagName("tbody")[0];
+
+    // Limpiar el contenido existente de la tabla
+    tbody.innerHTML = '';
+
+    // Iterar sobre el array de personas y agregar una fila por cada persona
+    for(let i=0;i <sessionStorage.length; i++){
+        let elemento = JSON.parse(sessionStorage.getItem(localStorage.key(i)));
+
+        var fila = document.createElement("tr");
+
+        // Agregar celdas con los datos de la persona
+        var celdaApellido = document.createElement("td");
+        celdaApellido.textContent = elemento.apellido;
+        fila.appendChild(celdaApellido);
+
+        var celdaNombre = document.createElement("td");
+        celdaNombre.textContent = elemento.nombre;
+        fila.appendChild(celdaNombre);
+
+        var celdaEdad = document.createElement("td");
+        celdaEdad.textContent = elemento.edad;
+        fila.appendChild(celdaEdad);
+
+        // Agregar la fila a la tabla
+        tbody.appendChild(fila);
+    }
+}
+
+mostrarTablaSession();
